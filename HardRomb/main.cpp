900b5fd45f2cd508aab5d1bf4ac50423dbe587c4
Sidor_md45@top-academy.ru <Sidor_md45@top-academy.ru>
2024-03-07 20:47:09 +0300

    HardChess2 DONE

---
+++ HardRomb/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+void main() {
+	setlocale(LC_ALL, "");
+	//int n;
+	//cout << "¬вдетие размер фигуры: "; cin >> n;
+
+
+	//for (int i = 0; i < n * 2; i++) {
+	//	for (int j = 0; j <= n * 2; j++) {
+	//		if (i < n - 1)cout << "\\";
+	//	}
+
+	//	cout << endl;
+	//}
+
+}
